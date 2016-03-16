730b90cb477ffcc54230c6c9880fcca51a612586
DevilBin <whu.evil.binary@gmail.com>
2016-03-16 14:34:40 +0800

    add 103

---
+++ 103.binaryTreeZigzagLevelOrderTraversal.cpp
@@ -0,0 +1,115 @@
+/*************************************************************************
+	> File Name: 103.binaryTreeZigzagLevelOrderTraversal.cpp
+	> Author: DevilBin 
+	> Mail: whu.evil.binary@gmail.com 
+	> Created Time: 2016/3/16 14:14:25
+ ************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <map>
+#include <algorithm>
+#include <sstream>
+#include <climits>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode * root) {
+        vector<vector<int>> result;
+        if(root == NULL) {
+            return result;
+        }
+        change(root, result);
+        return result;
+    }
+    void change(TreeNode * root, vector<vector<int>> & result) {
+        int flag = 0;
+        vector<int> intTemp = {root->val};
+        result.push_back(intTemp);
+        deque<TreeNode *> nodesTemp;
+        deque<deque<TreeNode *>> nodes = {{root}};
+        intTemp = {};
+        while(nodes.size() != 0 && nodes[0].size() != 0) {
+            TreeNode * head = nodes[0][0];
+            if(head->left != NULL) {
+                intTemp.push_back(head->left->val);
+                nodesTemp.push_back(head->left);
+            }
+            if(head->right != NULL) {
+                intTemp.push_back(head->right->val);
+                nodesTemp.push_back(head->right);
+            }
+            if(nodes[0].size() > 1) {
+                nodes[0].pop_front();
+            }
+            else {
+                if(intTemp.size() != 0) {
+                    if(flag == 1) {
+                        result.push_back(intTemp);
+                        intTemp = {};
+                        flag = 0;
+                    }
+                    else {
+                        reverse(intTemp.begin(), intTemp.end());
+                        result.push_back(intTemp);
+                        intTemp = {};
+                        flag = 1;
+                    }
+                }
+                nodes.push_back(nodesTemp);
+                nodes.pop_front();
+                nodesTemp = {};
+            }
+        }
+    }
+};
+
+void init(deque<TreeNode *> & nodes, const int arr[], int size) {
+    int i = 1;
+    while(i < size) {
+        if(arr[i] != '#') {
+            nodes[0]->left = new TreeNode(arr[i]);
+            nodes.push_back(nodes[0]->left);
+        }
+        i++;
+        if(arr[i] != '#') {
+            nodes[0]->right = new TreeNode(arr[i]);
+            nodes.push_back(nodes[0]->right);
+        }
+        i++;
+        nodes.pop_front();
+    }
+}
+
+int main() {
+    const int arr[] = {3, 9, 20, '#', '#', 15, 7};
+    TreeNode * root = new TreeNode(arr[0]);
+    deque<TreeNode *> nodes = {root};
+
+    init(nodes, arr, sizeof(arr) / sizeof(arr[0]));
+
+    Solution test;
+    vector<vector<int>> result = test.zigzagLevelOrder(root);
+    for(unsigned i = 0; i < result.size(); ++i) {
+        for(unsigned j = 0; j < result[i].size(); ++j) {
+            cout << result[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
